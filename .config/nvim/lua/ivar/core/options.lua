0f367427b23f7cda8a6ea2c6e5a28f93b3337e21
Ivar Åsell <ivar.asell@activesolution.se>
2024-03-14 22:44:18 +0100

    Start versioning nvim-lazyvim setup

--- .config/nvim/lua/ivar/core/options.lua
+++ .config/nvim/lua/ivar/core/options.lua
@@ -12,3 +12,17 @@ vim.o.splitright = true
 
 -- clipboard
 vim.o.clipboard = "unnamedplus"
+
+-- Tabs and indentations
+vim.o.tabstop = 2
+vim.bo.tabstop = 2
+vim.o.showtabline = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth = 2
+vim.bo.shiftwidth = 2
+vim.o.smartindent = true
+vim.bo.smartindent = true
+vim.o.autoindent = true
+vim.bo.autoindent = true
+vim.o.expandtab = true
+vim.bo.expandtab = true
