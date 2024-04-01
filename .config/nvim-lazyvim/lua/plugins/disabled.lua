04c3340b073716a1c0a24d486d6a30de2f2333e5
Ivar Åsell <ivar.asell@activesolution.se>
2024-04-01 19:43:10 +0200

    Not sure

---
+++ .config/nvim-lazyvim/lua/plugins/disabled.lua
@@ -0,0 +1,4 @@
+return {
+  { "folke/trouble.nvim", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+}
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,1 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
---require("ivar.core")
+require("ivar.core")
