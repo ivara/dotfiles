9edbb1cd645eda637d89bdf7eab20d4823e08572
Ivar Åsell <ivar.asell@activesolution.se>
2024-04-02 22:15:05 +0200

    A bit of changes

--- .config/nvim-lazyvim/lua/plugins/colorscheme.lua
+++ .config/nvim-lazyvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,18 @@
 return {
-  "rebelot/kanagawa.nvim",
-  config = function()
-    vim.cmd.colorscheme("kanagawa-wave")
-  end,
+
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
 }
+
+-- return {
+--   "rebelot/kanagawa.nvim",
+--   config = function()
+--     vim.cmd.colorscheme("kanagawa-wave")
+--   end,
+-- }
