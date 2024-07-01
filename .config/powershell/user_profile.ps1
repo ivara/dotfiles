a0bd69ca999e29f1b10eab69d0d145cfd98e37ef
Ivar Åsell <ivar.asell@activesolution.se>
2024-07-01 22:47:17 +0200

    powershell theme

--- .config/powershell/user_profile.ps1
+++ .config/powershell/user_profile.ps1
@@ -1,6 +1,6 @@
 # Prompt
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.json" | Invoke-Expression
-#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/atomic.omp.json" | Invoke-Expression
+#oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_rainbow.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/emodipt-extend.omp.json" | Invoke-Expression
 
 # PSReadLine
 Set-PSReadLineOption -EditMode vi
