ba65b8cda5456dadab13bc98eada0ae6875557c3
Ivar Åsell <ivar.asell@activesolution.se>
2024-09-12 13:58:45 +0200

    Beginning of a new beginning

---
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -0,0 +1,78 @@
+return {
+-- CATTPUCCIN
+{
+  "catppuccin/nvim",
+  lazy = false,
+  priority = 1000,
+  name = "catppuccin",
+  opts = {
+    integrations = {
+      aerial = true,
+      alpha = true,
+      cmp = true,
+      dashboard = true,
+      flash = true,
+      grug_far = true,
+      gitsigns = true,
+      headlines = true,
+      illuminate = true,
+      indent_blankline = { enabled = true },
+      leap = true,
+      lsp_trouble = true,
+      mason = true,
+      markdown = true,
+      mini = true,
+      native_lsp = {
+        enabled = true,
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      navic = { enabled = true, custom_bg = "lualine" },
+      neotest = true,
+      neotree = true,
+      noice = true,
+      notify = true,
+      semantic_tokens = true,
+      telescope = true,
+      treesitter = true,
+      treesitter_context = true,
+      which_key = true,
+    },
+  },
+},
+
+-- NORD Them
+    ---- NORD Themee
+{  'shaunsingh/nord.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    -- Example config in lua
+    vim.g.nord_contrast = true
+    vim.g.nord_borders = false
+    vim.g.nord_disable_background = true
+    vim.g.nord_italic = false
+    vim.g.nord_uniform_diff_background = true
+    vim.g.nord_bold = false
+
+    -- Load the colorscheme
+    require('nord').set()
+
+    -- Toggle background transparency
+    local bg_transparent = true
+
+    local toggle_transparency = function()
+      bg_transparent = not bg_transparent
+      vim.g.nord_disable_background = bg_transparent
+      vim.cmd [[colorscheme nord]]
+    end
+
+    vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true })
+  end,
+},
+
+}
