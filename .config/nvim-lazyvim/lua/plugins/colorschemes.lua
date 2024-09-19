98a4d2e565f6c255bfb84ec2327be5d03572a1bb
Ivar Åsell <ivar.asell@activesolution.se>
2024-09-19 09:59:29 +0200

    lazyvim configuration updates

--- .config/nvim-lazyvim/lua/plugins/colorschemes.lua
+++ .config/nvim-lazyvim/lua/plugins/colorschemes.lua
@@ -55,7 +55,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa-dragon",
+      colorscheme = "kanagawa",
     },
   },
 }
--- .config/nvim-lazyvim/lua/plugins/disabled.lua
+++ .config/nvim-lazyvim/lua/plugins/disabled.lua
@@ -4,4 +4,12 @@ return {
   { "rafamadriz/friendly-snippets", enabled = false },
   { "echasnovski/mini.surround", enabled = false },
   { "echasnovski/mini.bufremove", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  { "kristijanhusak/vim-dadbod-ui", enabled = false },
+  { "kristijanhusak/vim-dadbod-ui", enabled = false },
+  { "kristijanhusak/vim-dadbod-completion", enabled = false },
+  { "folke/edgy.nvim", enabled = false },
+
+  { "garymjr/nvim-snippets", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
 }
