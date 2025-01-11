52d65e64ed6535aa746347c3bf99431457478c77
Ivar Åsell <ivar.asell@activesolution.se>
2025-01-11 14:18:26 +0100

    add vim-tmux-navigator

--- .config/nvim-lazyvim/lua/plugins/neotest.lua
+++ .config/nvim-lazyvim/lua/plugins/neotest.lua
@@ -1,5 +1,9 @@
 return {
-  { "Issafalcon/neotest-dotnet" },
+  -- { "Issafalcon/neotest-dotnet" },
+  {
+    "Issafalcon/neotest-dotnet",
+    commit = "184f9f5c5e39b87025eebbbeed7736200338377c",
+  },
   {
     "nvim-neotest/neotest",
     dependencies = { "Issafalcon/neotest-dotnet" },
