2ee991ccc65dd9d536d5f700cd6030c0451772c8
Ivar Åsell <ivar.asell@activesolution.se>
2025-04-09 19:47:53 +0200

    misc

--- nvim-lazyvim/.config/nvim-lazyvim/lua/config/keymaps.lua
+++ nvim-lazyvim/.config/nvim-lazyvim/lua/config/keymaps.lua
@@ -5,3 +5,16 @@
 local map = vim.api.nvim_set_keymap
 
 map("i", "jk", "<Esc>", { noremap = true, silent = false })
+
+-- Move a line up and down in normal mode
+-- map("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
+-- map("n", "<A-k>", ":m .-2<CR>==", { noremap = true, silent = true })
+-- vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+-- vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Move selected lines up and down in visual mode
+-- map("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+-- map("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+--
+-- map("v", "<M-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+-- map("v", "<M-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
+-- map("n", "<M-a>", ":lua print('hello')", { noremap = true, silent = true })
