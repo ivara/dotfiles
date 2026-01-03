a929329c08bee9085f7f473432e748636fe3bb30
Ivar Åsell <ivar.asell@activesolution.se>
2026-01-03 12:57:37 +0100

    formatting

--- nvim-2026/.config/nvim-2026/lua/plugins/lazygit.lua
+++ nvim-2026/.config/nvim-2026/lua/plugins/lazygit.lua
@@ -1,22 +1,22 @@
 return {
-	"kdheepak/lazygit.nvim",
-	cmd = {
-		"LazyGit",
-		"LazyGitConfig",
-		"LazyGitCurrentFile",
-		"LazyGitFilter",
-		"LazyGitFilterCurrentFile",
-	},
-	keys = {
-		{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		{ "<leader>gf", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit current file" },
-	},
-	-- optional dependency for floating window border decoration
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		vim.g.lazygit_floating_window_scaling_factor = 0.94 -- 90% of screen
-		vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
-	end,
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  keys = {
+    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+    { "<leader>gf", "<cmd>LazyGitCurrentFile<cr>", desc = "LazyGit current file" },
+  },
+  -- optional dependency for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    vim.g.lazygit_floating_window_scaling_factor = 0.94 -- 90% of screen
+    vim.g.lazygit_floating_window_border_chars = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" }
+  end,
 }
