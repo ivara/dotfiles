a2631693c20d6df8b392331e20f3e187bded0aae
Ivar Åsell <ivar.asell@activesolution.se>
2026-01-05 00:06:26 +0100

    update nvim conf

--- nvim-2026/.config/nvim-2026/lua/plugins/mini.lua
+++ nvim-2026/.config/nvim-2026/lua/plugins/mini.lua
@@ -12,9 +12,37 @@ return {
       -- Icons: icon provider for other mini modules
       require("mini.icons").setup()
 
-      -- Statusline: minimal and fast statusline
+      -- Statusline: minimal and fast statusline with macro recording indicator
       require("mini.statusline").setup({
         use_icons = true,
+        content = {
+          active = function()
+            local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+            local git           = MiniStatusline.section_git({ trunc_width = 40 })
+            local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
+            local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+            local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
+            local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+            local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+            local location      = MiniStatusline.section_location({ trunc_width = 75 })
+            local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+            -- Macro recording indicator (shown in orange/warning color)
+            local recording = vim.fn.reg_recording()
+            local macro = recording ~= "" and ("Recording @" .. recording) or ""
+
+            return MiniStatusline.combine_groups({
+              { hl = mode_hl,                  strings = { mode } },
+              { hl = "WarningMsg",             strings = { macro } },
+              { hl = "MiniStatuslineDevinfo",  strings = { git, diff, diagnostics, lsp } },
+              "%<", -- Mark general truncate point
+              { hl = "MiniStatuslineFilename", strings = { filename } },
+              "%=", -- End left alignment
+              { hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+              { hl = mode_hl,                  strings = { search, location } },
+            })
+          end,
+        },
       })
 
       -- Tabline: show buffers at the top
@@ -46,6 +74,76 @@ return {
       -- Buffer navigation
       vim.keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Previous buffer" })
       vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next buffer" })
+
+      -- Clue: keybinding discovery (replacement for which-key)
+      local miniclue = require("mini.clue")
+      miniclue.setup({
+        triggers = {
+          -- Leader triggers
+          { mode = "n", keys = "<Leader>" },
+          { mode = "x", keys = "<Leader>" },
+
+          -- Built-in completion
+          { mode = "i", keys = "<C-x>" },
+
+          -- `g` key
+          { mode = "n", keys = "g" },
+          { mode = "x", keys = "g" },
+
+          -- Marks
+          { mode = "n", keys = "'" },
+          { mode = "n", keys = "`" },
+          { mode = "x", keys = "'" },
+          { mode = "x", keys = "`" },
+
+          -- Registers
+          { mode = "n", keys = '"' },
+          { mode = "x", keys = '"' },
+          { mode = "i", keys = "<C-r>" },
+          { mode = "c", keys = "<C-r>" },
+
+          -- Window commands
+          { mode = "n", keys = "<C-w>" },
+
+          -- `z` key
+          { mode = "n", keys = "z" },
+          { mode = "x", keys = "z" },
+
+          -- Brackets
+          { mode = "n", keys = "[" },
+          { mode = "n", keys = "]" },
+        },
+
+        clues = {
+          -- Enhance with built-in clues
+          miniclue.gen_clues.builtin_completion(),
+          miniclue.gen_clues.g(),
+          miniclue.gen_clues.marks(),
+          miniclue.gen_clues.registers(),
+          miniclue.gen_clues.windows(),
+          miniclue.gen_clues.z(),
+
+          -- Custom group descriptions
+          { mode = "n", keys = "<Leader>f", desc = "+find" },
+          { mode = "n", keys = "<Leader>g", desc = "+git" },
+          { mode = "n", keys = "<Leader>c", desc = "+code" },
+          { mode = "n", keys = "<Leader>d", desc = "+diagnostics" },
+          { mode = "n", keys = "<Leader>u", desc = "+ui/toggle" },
+          { mode = "n", keys = "<Leader>ud", desc = "+diagnostics" },
+          { mode = "n", keys = "<Leader>x", desc = "+quickfix" },
+          { mode = "n", keys = "<Leader>h", desc = "+hunk/git" },
+          { mode = "n", keys = "<Leader>t", desc = "+toggle" },
+          { mode = "n", keys = "<Leader>b", desc = "+buffer" },
+        },
+
+        -- window = {
+        --   delay = 300,
+        --   config = {
+        --     width = "auto",
+        --     border = "rounded",
+        --   },
+        -- },
+      })
     end,
   },
 }
--- nvim-2026/.config/nvim-2026/lua/plugins/which-key.lua
+++ nvim-2026/.config/nvim-2026/lua/plugins/which-key.lua
@@ -1,35 +1,4 @@
--- which-key: Keybinding discovery
--- https://github.com/folke/which-key.nvim
-return {
-  {
-    'folke/which-key.nvim',
-    event = 'VeryLazy',
-    opts = {
-      preset = 'modern',
-      delay = 300,
-      icons = {
-        breadcrumb = '',
-        separator = '',
-        group = ' ',
-      },
-      spec = {
-        { '<leader>f', group = 'find' },
-        { '<leader>g', group = 'git' },
-        { '<leader>c', group = 'code' },
-        { '<leader>d', group = 'diagnostics' },
-        { '<leader>u', group = 'ui/toggle' },
-        { '<leader>ud', group = 'diagnostics' },
-        { '<leader>x', group = 'quickfix' },
-      },
-    },
-    keys = {
-      {
-        '<leader>?',
-        function()
-          require('which-key').show({ global = false })
-        end,
-        desc = 'Buffer local keymaps',
-      },
-    },
-  },
-}
+-- mini.clue: Keybinding discovery (replacing which-key)
+-- Part of mini.nvim: https://github.com/echasnovski/mini.nvim
+-- Note: mini.clue is configured in mini.lua since we already load mini.nvim
+return {}
