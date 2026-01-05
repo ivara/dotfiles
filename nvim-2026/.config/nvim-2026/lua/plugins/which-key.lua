2e1c6aa3d431896cf55ff129ef97d7560a0836a4
Ivar Åsell <ivar.asell@activesolution.se>
2026-01-06 00:30:07 +0100

    remove whichkey in favor of mini.clues

--- nvim-2026/.config/nvim-2026/lua/plugins/which-key.lua
+++
@@ -1,4 +0,0 @@
--- mini.clue: Keybinding discovery (replacing which-key)
--- Part of mini.nvim: https://github.com/echasnovski/mini.nvim
--- Note: mini.clue is configured in mini.lua since we already load mini.nvim
-return {}
